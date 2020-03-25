f657033c0cc63b5653d94656b882a52ea9aa63a7
Vincenzo <vincenzolucente@gmail.com>
2020-03-25 11:27:13 +0100

    use enviroment variables

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,16 +1,21 @@
 package guru.springframework.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 import guru.springframework.examplebeans.FakeDataSource;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
+	
+	@Autowired
+	Environment env;
 	
 	@Value("${guru.username}")
 	String user;
@@ -19,16 +24,16 @@ public class PropertyConfig {
 	@Value("${guru.db.url}")
 	String url;
 	
+	
+	
 	@Bean
 	public FakeDataSource fakeDataSource() {
 		FakeDataSource fakeDataSource = new FakeDataSource();
-		fakeDataSource.setUser(user);
+		fakeDataSource.setUser(env.getProperty("USERNAME"));
 		fakeDataSource.setPassword(password);
 		fakeDataSource.setUrl(url);
 		return fakeDataSource;
 	}
-
-	
 
 	@Bean
 	//This enable us to read the the properties from the file
